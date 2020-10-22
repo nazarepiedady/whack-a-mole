e58696719dff9512f5ab03adf574dd3c8632c489
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-10-22 09:07:59 +0100

    Create `randomSquare` function
    Add a function that'll running over all element into *squareList*
    and remove for all them the class named **mole**, then select a
    random element of the *squareList* and store into a let variable
    named *randomSquare* for add the class named **mole**. In the end
    store the id of the *randomSquare* element into hitPosition variable
    to use it later.

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const $timeLeft = document.querySelector('#time-left');
 let $score = document.querySelector('#score');
 
 let result = 0;
+
+function randomSquare() {
+  squareList.forEach($square => { $square.classList.remove('mole') });
+  let $randomSquare = squareList[Math.floor(Math.random() * 9)];
+  $randomSquare.classList.add('mole');
+  
+  // assign the id of the randomSquare to hitPosition for us to use later
+  hitPosition = $randomSquare.id;
+}
