271d73b17b271baa36303366bf3b931dc772009c
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-10-22 08:54:53 +0100

    Initialize variables
    Initialize variables selecting all markup element that'll be part
    of program and create a variable called `result` with javascript
    keyword let that'll be store all points maded during the program

---
+++ app.js
@@ -0,0 +1,6 @@
+const squareList = document.querySelectorAll('.square');
+const moleList = document.querySelectorAll('.mole');
+const $timeLeft = document.querySelector('#time-left');
+let $score = document.querySelector('#score');
+
+let result = 0;
